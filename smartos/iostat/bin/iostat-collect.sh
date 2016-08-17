eec79ea77610576650c6ee87ad1a1491a3145b88
Alexandr Logvinov <kayama@siroplab.com>
2016-08-17 10:26:20 +0300

    Add smartos iostat-collect.sh

--- smartos/iostat/bin/iostat-collect.sh
+++ smartos/iostat/bin/iostat-collect.sh
@@ -6,5 +6,6 @@ IOSTAT="/usr/bin/iostat"
 
 [[ $# -lt 2 ]] && { echo "FATAL: some parameters not specified"; exit 1; }
 
-$IOSTAT -x 1 $SECONDS | grep -v 'extended' | awk 'BEGIN {check=0;} {if(check==1 && $1!=""){print $0}if($1=="device"){check=1}}' > $TOFILE 
+$IOSTAT -x 1 $SECONDS | grep -v 'extended' | awk 'BEGIN {check=0;} {if(check==1 && $1!=""){print $0}if($1=="device"){check=1}}' > $TOFILE.new 
+mv $TOFILE.new $TOFILE
 echo 0
