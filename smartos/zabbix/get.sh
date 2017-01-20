412aa9f0b5a0f8bff2894fc257bdac9648076032
Alexandr Logvinov <kayama@siroplab.com>
2017-01-20 15:00:40 +0200

    Add sed to SmartOS conf

--- smartos/zabbix/get.sh
+++ smartos/zabbix/get.sh
@@ -3,6 +3,9 @@ wget http://www.zabbix.com/downloads/3.0.4/zabbix_agents_3.0.4.solaris10.amd64.t
 mkdir /opt/zabbix
 tar -zxvf zabbix_agents_3.0.4.solaris10.amd64.tar.gz -C /opt/zabbix/
 
+sed -i 's/Server=127.0.0.1/Server=example.com/g' /opt/zabbix/conf/zabbix_agentd.conf 
+sed -i 's/ServerActive=127.0.0.1/ServerActive=example.com/g' /opt/zabbix/conf/zabbix_agentd.conf 
+
 mkdir /opt/zabbix/etc
 ln -s /opt/zabbix/conf/zabbix_agentd.conf /opt/zabbix/etc/zabbix_agentd.conf
 
